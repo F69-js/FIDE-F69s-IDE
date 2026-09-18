e796642c5ce15e5349418858a571b26356c7e1b7
F69.js <ikoi22323@outlook.jp>
2026-09-18 23:10:28 +0900

    Update highlighter.js

--- scripts/highlighter.js
+++ scripts/highlighter.js
@@ -1,9 +1,57 @@
-// FIDE Custom IDE - Syntax Highlighter Module (Universal Version)
+// FIDE Custom IDE - Syntax Highlighter Module (Ultimate Encyclopedia Version)
 const K_HL = {
-  'k': ["if","else","switch","case","break","return","continue","typeof","instanceof","throw","for","let","const","var","class","export","constructor","new","import","from","try","catch","in","async","await","default","do","while","yield","function"],
-  's': ["this","window","globalThis"],
-  'b': ["JSON","console","Math","Date","Temporal","Promise","String","Map","Set","Object","Number","Error","Array","ObservableString","BlockTypes","Player","undefined","null","true","false","parse","stringify","log","warn","error","floor","ceil","abs","max","min","pow","parseFloat","parseInt","isNaN","toString","keys","random","now"],
-  'm': ["push","pop","unshift","shift","slice","splice","filter","some","findIndex","includes","join","split","match","replace","replaceAll","trim","startsWith","indexOf","lastIndexOf","substring","map","forEach","reduce","padStart","toFixed","has","get","set","delete","entries","add","hasOwnProperty","subscribe","then","repeat","next"]
+  // 制御構文・予約語（Keyword / Control）
+  'k': [
+    "if","else","switch","case","break","return","continue","typeof","instanceof","throw","for","let","const","var","class","export","constructor","new","import","from","try","catch","in","async","await","default","do","while","yield","function",
+    "extends","super","finally","with","debugger","arguments","interface","implements","package","private","protected","public","static"
+  ],
+  
+  // 特殊参照（Special References）
+  's': [
+    "this","window","globalThis","super","self","global","screenLeft","screenTop"
+  ],
+  
+  // 標準・実験的・歴史的オブジェクト＆グローバル関数（Built-in Objects / Global Functions / Framework Globals）
+  'b': [
+    "JSON","console","Math","Date","Promise","String","Map","Set","Object","Number","Error","undefined","null","true","false",
+    "Boolean","RegExp","Function","Symbol","Proxy","Reflect","BigInt","URL","URLSearchParams","WeakMap","WeakSet","ArrayBuffer","DataView",
+    "Uint8Array","Float64Array","Int32Array","Int8Array","Uint16Array","Int16Array","Uint32Array","Float32Array","BigInt64Array","BigUint64Array",
+    "TypeError","ReferenceError","SyntaxError","RangeError","URIError","AggregateError","EvalError",
+    "Atomics","FinalizationRegistry","WeakRef","Intl","Collator","DateTimeFormat","NumberFormat","PluralRules","RelativeTimeFormat","ListFormat","Locale","DisplayNames","Segmenter",
+    "process","document","navigator","screen","location","history",
+    "Temporal","LanguageModel","ai",
+    "InternalError","ParallelArray","StopIteration",
+    "eval","escape","unescape",
+    "$","_","jQuery","React","ReactDOM","Vue","Angular","Rx"
+  ],
+  
+  // メソッド・プロパティ名（Methods / Properties）
+  'm': [
+    "push","pop","unshift","shift","slice","splice","filter","some","findIndex","includes","join","split","match","replace","replaceAll","trim","startsWith","indexOf","lastIndexOf","substring","map","forEach","reduce","padStart","toFixed","has","get","set","delete","entries","add","hasOwnProperty","subscribe","then","repeat","next",
+    "parse","stringify","log","warn","error","floor","ceil","abs","max","min","pow","parseFloat","parseInt","isNaN","toString","keys","random","now",
+    "find","findLast","findLastIndex","reduceRight","every","sort","reverse","flat","flatMap","fill","values","endsWith","toLowerCase","toUpperCase","matchAll","search","concat","padEnd","charAt","charCodeAt","catch","finally","valueOf","apply","call","bind","setTimeout","setInterval","clearTimeout","clearInterval","addEventListener","removeEventListener","dispatchEvent","preventDefault","stopPropagation","fetch",
+    "assign","create","defineProperty","freeze","seal","is","all","race","any","allSettled","resolve","reject","fromEntries","ownKeys","getOwnPropertyDescriptor","getOwnPropertyDescriptors","getPrototypeOf","setPrototypeOf","isExtensible","preventExtensions",
+    "E","LN10","LN2","LOG10E","LOG2E","PI","SQRT1_2","SQRT2",
+    "toReversed","toSorted","toSpliced","with",
+    "anchor","big","blink","bold","fixed","fontcolor","fontsize","italics","link","small","strike","sub","sup",
+    "clz32","imul","sign","log10","log2","log1p","expm1","cosh","sinh","tanh","acosh","asinh","atanh","hypot","trunc","fround","cbrt",
+    "getUint8","setUint8","getInt8","setInt8","getUint16","setUint16","getInt16","setInt16","getUint32","setUint32","getInt32","setInt32","getFloat32","setFloat32","getFloat64","setFloat64","getBigInt64","setBigInt64","getBigUint64","setBigUint64",
+    "deref","register","unregister","supportedLocalesOf","compare","format","formatToParts","resolvedOptions","select","pluralRule","v3",
+    "load","polyfill","isLockFree","wait","notify","waitAsync",
+    "codePointAt","normalize","trimStart","trimEnd","trimLeft","trimRight","isPrototypeOf","propertyIsEnumerable","toLocaleString","toSource","toPrecision","toExponential",
+    "Instant","Duration","PlainDate","PlainTime","PlainDateTime","ZonedDateTime","Calendar","TimeZone",
+    "until","since","round","equals","withPlainDate","withPlainTime",
+    "year","month","day","hour","minute","second","millisecond","microsecond","nanosecond",
+    "languageModel","capabilities","availability","prompt","promptStreaming","destroy","tokensLeft","maxTokens","tokensSoFar",
+    "getYear","setYear","toGMTString","__proto__","__defineGetter__","__defineSetter__","__lookupGetter__","__lookupSetter__","compile","observe","unobserve","caller","callee",
+    "useState","useEffect","useContext","useReducer","useCallback","useMemo","useRef","useLayoutEffect",
+    "createRoot","render","hydrateRoot",
+    "createApp","ref","reactive","computed","watch","watchEffect","onMounted","onUnmounted","onUpdated",
+    "component","directive","mixin","provide","inject",
+    "ajax","animate","css","html","text","val","on","off","trigger","attr","removeAttr","addClass","removeClass","toggleClass","find","closest","parent","children","siblings",
+    "debounce","throttle","cloneDeep","merge","extend","pick","omit","uniq","flatten","delay","memoize",
+    "of","fromEvent","combineLatest","forkJoin","switchMap","mergeMap","concatMap","catchError","pipe","observable"
+  ]
 };
 
 function runHl(t) {
@@ -23,8 +71,8 @@ function runHl(t) {
       res += c.replace(/</g, '&lt;').replace(/>/g, '&gt;');
       if (c === sC) { res += '</span>'; s = 0 } idx++; continue;
     }
-    if (c === '/' && text=t, text[idx + 1] === '/') { flush(); res += '<span class="c">//'; c1 = 1; idx += 2; continue }
-    if (c === '/' && text=t, text[idx + 1] === '*') { flush(); res += '<span class="c">/*'; c2 = 1; idx += 2; continue }
+    if (c === '/' && t[idx + 1] === '/') { flush(); res += '<span class="c">//'; c1 = 1; idx += 2; continue }
+    if (c === '/' && t[idx + 1] === '*') { flush(); res += '<span class="c">/*'; c2 = 1; idx += 2; continue }
     if (c === "'" || c === '"' || c === '`') { flush(); sC = c; res += `<span class="str">${c}`; s = 1; idx++; continue }
     
     if (/[a-zA-Z0-9_*]/.test(c)) { 
