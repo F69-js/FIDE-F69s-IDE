20df6a237e0cbcd641b00e5c709066becff22a75
F69.js <ikoi22323@outlook.jp>
2026-09-19 14:17:20 +0900

    Create gateway.js

---
+++ scripts/newtacs/langs/gateway.js
@@ -0,0 +1,53 @@
+// FIDE Tacs Highlighter© - Central Language Gateway Hub (v1.0)
+// 💡 全23言語のハイライト関数と言語CSSテーマパレットをここに一括集約して再エクスポート！
+
+import { ApplyHighlighttoJS, JStheme } from "./js.js";
+import { ApplyHighlighttoJSON, JSONtheme } from "./json.js";
+import { ApplyHighlighttoHTML, HTMLtheme } from "./html.js";
+import { ApplyHighlighttoCSS, CSStheme } from "./css.js";
+import { ApplyHighlighttoMD, MDtheme } from "./md.js";
+import { ApplyHighlighttoPY, PYtheme } from "./py.js";
+import { ApplyHighlighttoPHP, PHPtheme } from "./php.js";
+import { ApplyHighlighttoCPP, CPPtheme } from "./cpp.js";
+import { ApplyHighlighttoCS, CStheme } from "./cs.js";
+import { ApplyHighlighttoJAVA, JAVAtheme } from "./java.js";
+import { ApplyHighlighttoTS, TStheme } from "./ts.js";
+import { ApplyHighlighttoSQL, SQLtheme } from "./sql.js";
+import { ApplyHighlighttoSH, SHtheme } from "./sh.js";
+import { ApplyHighlighttoYAML, YAMLtheme } from "./yaml.js";
+import { ApplyHighlighttoTOML, TOMLtheme } from "./toml.js";
+import { ApplyHighlighttoRust, Rusttheme } from "./rust.js";
+import { ApplyHighlighttoGo, Gotheme } from "./go.js";
+import { ApplyHighlighttoRuby, Rubytheme } from "./ruby.js";
+import { ApplyHighlighttoKT, KTtheme } from "./kt.js";
+import { ApplyHighlighttoSwift, Swifttheme } from "./swift.js";
+import { ApplyHighlighttoDart, Darttheme } from "./dart.js";
+import { ApplyHighlighttoR, Rtheme } from "./r.js";
+import { ApplyHighlighttoDocker, Dockertheme } from "./docker.js";
+
+// 💡 外部のエディタコアや親玉ファイルに向けて、すべてのモジュールをそのまま大解放（再エクスポート）！
+export {
+  ApplyHighlighttoJS, JStheme,
+  ApplyHighlighttoJSON, JSONtheme,
+  ApplyHighlighttoHTML, HTMLtheme,
+  ApplyHighlighttoCSS, CSStheme,
+  ApplyHighlighttoMD, MDtheme,
+  ApplyHighlighttoPY, PYtheme,
+  ApplyHighlighttoPHP, PHPtheme,
+  ApplyHighlighttoCPP, CPPtheme,
+  ApplyHighlighttoCS, CStheme,
+  ApplyHighlighttoJAVA, JAVAtheme,
+  ApplyHighlighttoTS, TStheme,
+  ApplyHighlighttoSQL, SQLtheme,
+  ApplyHighlighttoSH, SHtheme,
+  ApplyHighlighttoYAML, YAMLtheme,
+  ApplyHighlighttoTOML, TOMLtheme,
+  ApplyHighlighttoRust, Rusttheme,
+  ApplyHighlighttoGo, Gotheme,
+  ApplyHighlighttoRuby, Rubytheme,
+  ApplyHighlighttoKT, KTtheme,
+  ApplyHighlighttoSwift, Swifttheme,
+  ApplyHighlighttoDart, Darttheme,
+  ApplyHighlighttoR, Rtheme,
+  ApplyHighlighttoDocker, Dockertheme
+};
